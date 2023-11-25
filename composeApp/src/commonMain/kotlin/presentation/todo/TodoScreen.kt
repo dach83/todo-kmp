9bba40b7bc368023c27025b9c62669fed513d5fb
dach83 <dach83@mail.ru>
2023-11-26 01:19:51 +0500

    Coloring safe area iOs

--- composeApp/src/commonMain/kotlin/presentation/todo/TodoScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/todo/TodoScreen.kt
@@ -1,14 +1,16 @@
 package presentation.todo
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
@@ -29,20 +31,27 @@ fun TodoScreen(
                 onClick = { },
                 contentColor = Color.White,
                 backgroundColor = MaterialTheme.colors.primary,
-                shape = RoundedCornerShape(16.dp)
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.navigationBarsPadding()
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
                     contentDescription = null
                 )
             }
-        }
+        },
+        backgroundColor = MaterialTheme.colors.secondary
     ) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-                .background(MaterialTheme.colors.secondary)
-        )
+                .systemBarsPadding()
+        ) {
+            Text(
+                text = "test",
+                color = Color.White
+            )
+        }
     }
 }
