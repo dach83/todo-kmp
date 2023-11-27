190fb97cf2a3ef1ff31faf86c637dcc21dd2d887
dach83 <dach83@mail.ru>
2023-11-27 23:59:24 +0500

    Todos lazy list

--- composeApp/src/commonMain/kotlin/presentation/todo/TodoScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/todo/TodoScreen.kt
@@ -1,16 +1,34 @@
 package presentation.todo
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -23,7 +41,11 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,7 +54,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import domain.model.TodoEntity
 import org.koin.compose.koinInject
@@ -41,7 +65,7 @@ import org.koin.compose.koinInject
 fun TodoScreen(
     viewModel: TodoViewModel = koinInject()
 ) {
-    // val state = viewModel.uiState
+    val state by viewModel.uiState.collectAsState()
     var dialogOpen by remember { mutableStateOf(false) }
     if (dialogOpen) {
         AddTodoDialog(
@@ -72,12 +96,169 @@ fun TodoScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .systemBarsPadding()
+                .systemBarsPadding(),
+            contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "test",
-                color = Color.White
+            AnimatedVisibility(
+                visible = state.todos.isEmpty(),
+                enter = scaleIn() + fadeIn(),
+                exit = scaleOut() + fadeOut()
+            ) {
+                Text(
+                    text = "No Todos Yet!",
+                    color = Color.White,
+                    fontSize = 22.sp
+                )
+            }
+
+            AnimatedVisibility(
+                visible = state.todos.isNotEmpty(),
+                enter = scaleIn() + fadeIn(),
+                exit = scaleOut() + fadeOut()
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            start = 8.dp,
+                            end = 8.dp,
+                            top = 8.dp,
+                            bottom = paddingValues.calculateBottomPadding() + 8.dp
+                        ),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(
+                        items = state.todos.sortedBy { it.done },
+                        key = { it.id }
+                    ) { todo ->
+                        TodoItem(
+                            todo = todo,
+                            onClick = {
+                                viewModel.updateTodo(
+                                    todo.copy(done = !todo.done)
+                                )
+                            },
+                            onDelete = {
+                                viewModel.deleteTodo(todo)
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LazyItemScope.TodoItem(
+    todo: TodoEntity,
+    onClick: () -> Unit,
+    onDelete: () -> Unit
+) {
+    val color by animateColorAsState(
+        targetValue = if (todo.done) {
+            Color(0xff24d65f)
+        } else {
+            Color(0xffff6363)
+        },
+        animationSpec = tween(500)
+    )
+
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateItemPlacement(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
             )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(5.dp))
+                .background(color)
+                .clickable {
+                    onClick()
+                }
+                .padding(
+                    horizontal = 8.dp,
+                    vertical = 8.dp
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(25.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colors.primary)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Row {
+                    AnimatedVisibility(
+                        visible = todo.done,
+                        enter = scaleIn() + fadeIn(),
+                        exit = scaleOut() + fadeOut()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = null,
+                            tint = color
+                        )
+                    }
+                }
+                Row {
+                    AnimatedVisibility(
+                        visible = !todo.done,
+                        enter = scaleIn() + fadeIn(),
+                        exit = scaleOut() + fadeOut()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null,
+                            tint = color
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = todo.title,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = todo.subtitle,
+                    fontSize = 12.sp,
+                    color = Color(0xffebebeb)
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(25.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colors.primary)
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    tint = color,
+                    modifier = Modifier.clickable {
+                        onDelete()
+                    }
+                )
+            }
         }
     }
 }
@@ -94,7 +275,7 @@ private fun AddTodoDialog(
             modifier = Modifier
                 .clip(RoundedCornerShape(8.dp))
                 .background(MaterialTheme.colors.primary)
-                .padding(8.dp),
+                .padding(12.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -139,7 +320,9 @@ private fun AddTodoDialog(
                 },
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = MaterialTheme.colors.secondary
-                )
+                ),
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text = "Submit",
